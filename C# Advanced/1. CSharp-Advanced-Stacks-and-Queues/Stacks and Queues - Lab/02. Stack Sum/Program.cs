e450b4f485db4375f2aae7e3d57aa0514105b64e
Yordan Valkov <yordan.valkov@gmail.com>
2022-04-09 23:07:21 +0300

    C# Advanced

---
+++ C# Advanced/1. CSharp-Advanced-Stacks-and-Queues/Stacks and Queues - Lab/02. Stack Sum/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._Stack_Sum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            Stack<int> stackSum = new Stack<int>(numbers);
+            string cmd;
+
+            while ((cmd = Console.ReadLine().ToUpper()) != "END")
+            {
+                string[] array = cmd.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                string action = array[0];
+                if (action == "ADD")
+                {
+                    int first = int.Parse(array[1]);
+                    int second = int.Parse(array[2]);
+
+                    stackSum.Push(first);
+                    stackSum.Push(second);
+                }
+                if (action == "REMOVE")
+                {
+                    int count = int.Parse(array[1]);
+
+                    if (stackSum.Count >= count)
+                    {
+                        for (int i = 0; i < count; i++)
+                        {
+                            stackSum.Pop();
+                        }
+                    }     
+                }
+            }
+            Console.WriteLine($"Sum: {stackSum.Sum()}");
+        }
+    }
+}
---
+++ C# Advanced/1. CSharp-Advanced-Stacks-and-Queues/Stacks and Queues - Lab/05. Hot Potato/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _05._Hot_Potato
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] boy = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            int n = int.Parse(Console.ReadLine());
+            Queue<string> result = new Queue<string>(boy);
+
+            while (result.Count > 1)
+            {
+                for (int k = 1; k < n; k++)
+                {
+                    result.Enqueue(result.Dequeue());
+                }
+
+                Console.WriteLine($"Removed {result.Dequeue()}");
+            }
+            Console.WriteLine($"Last is {result.Dequeue()}");
+        }
+    }
+}
