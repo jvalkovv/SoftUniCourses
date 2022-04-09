3929e5b48dde01d29a2ebd176bfdd89b2c872adf
Yordan Valkov <yordan.valkov@gmail.com>
2022-04-09 23:25:07 +0300

    C# Advanced

---
+++ C# Advanced/1. Stacks and Queues/Stacks and Queues - Lab/01. Reverse Strings/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _1._Reverse_Strings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string input = Console.ReadLine();
+
+            Stack<char> stack = new Stack<char>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                stack.Push(input[i]);
+            }
+            for (int i = 0; i < input.Length; i++)
+            {
+                Console.Write(stack.Pop());
+            }
+
+        }
+    }
+}
---
+++ C# Advanced/1. Stacks and Queues/Stacks and Queues - Lab/03. Decimal to Binary Converter/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03._Decimal_to_Binary_Converter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            List<int> list = new List<int>();
+
+            if (number == 0)
+            {
+                Console.WriteLine("0");
+            }
+
+            while (number > 0)
+            {
+                if (number % 2 != 0)
+                {
+                    int remainder = number % 2;
+                    number /= 2;
+                    list.Add(remainder);
+                }
+                else if (number % 2 == 0)
+                {
+                    int remainder = number % 2;
+                    number /= 2;
+                    list.Add(remainder);
+                }
+            }
+          
+            Stack<int> results = new Stack<int>(list);
+
+            while (results.Count > 0)
+            {
+                Console.Write(results.Pop());
+            }
+        }
+    }
+}
