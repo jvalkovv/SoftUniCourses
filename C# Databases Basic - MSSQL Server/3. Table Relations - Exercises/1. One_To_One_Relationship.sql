a5c27f5134f8fc102a3da5195317cb9c38a57c6c
Yordan Valkov <yordan.valkov@gmail.com>
2022-10-07 12:09:16 +0300

    C# Databases module MSSQL

---
+++ C# Databases Basic - MSSQL Server/3. Table Relations - Exercises/1. One_To_One_Relationship.sql
@@ -0,0 +1,23 @@
+CREATE TABLE Passports
+(
+PassportID INT PRIMARY KEY IDENTITY(101,1),
+PassportNumber CHAR(10)
+)
+
+CREATE TABLE Persons
+(
+ PersonID INT PRIMARY KEY IDENTITY,
+ FirstName NVARCHAR(30),
+ Salary DECIMAL(15,2),
+ PassportID INT UNIQUE FOREIGN KEY REFERENCES Passports(PassportID)
+)
+
+INSERT INTO Passports (PassportNumber) VALUES
+('N34FG21B'),
+('K65LO4R7'),
+('ZE657QP2')
+
+INSERT INTO Persons (FirstName, Salary, PassportID) VALUES
+('Roberto', 43300, 102),
+('Tom', 56100.00, 103),
+('Yana', 60200.00, 101)
