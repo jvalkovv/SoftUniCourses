c3df0fbf2c6c5355ef041e5f548818ee52a8d11a
Yordan Valkov <yordan.valkov@gmail.com>
2022-10-16 20:28:47 +0300

    C# OOP Inheritance

---
+++ C# OOP/01.Inheritance/Inheritance - Lab/3. Hierarchical Inheritance/Animal.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace Farm
+{
+    public class Animal
+    {
+        public void Eat()
+        {
+            Console.WriteLine("eating...");
+        }
+    }
+}
