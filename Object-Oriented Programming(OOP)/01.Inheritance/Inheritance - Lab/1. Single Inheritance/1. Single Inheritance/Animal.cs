6f7676d100579ba1c9c2b158b667cf65485f976b
Yordan Valkov <yordan.valkov@gmail.com>
2022-10-16 19:51:51 +0300

    C# OOP

---
+++ Object-Oriented Programming(OOP)/01.Inheritance/Inheritance - Lab/1. Single Inheritance/1. Single Inheritance/Animal.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _1._Single_Inheritance
+{
+    public class Animal
+    {
+        public void Eat()
+        {
+            Console.WriteLine("eating...");
+        }
+
+    }
+}
---
+++ Object-Oriented Programming(OOP)/01.Inheritance/Inheritance - Lab/1. Single Inheritance/1. Single Inheritance/Dog.cs
@@ -0,0 +1,15 @@
+﻿using _1._Single_Inheritance;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Farm
+{
+    public  class Dog : Animal
+    {
+        public void Bark()
+        {
+            Console.WriteLine("barking...");
+        }
+    }
+}
