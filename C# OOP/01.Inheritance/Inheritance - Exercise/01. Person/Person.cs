154dcc29a5814bedbc3fc1cb4040b3cb90aeb85c
Yordan Valkov <yordan.valkov@gmail.com>
2022-10-17 16:01:24 +0300

    C# OOP

---
+++ C# OOP/01.Inheritance/Inheritance - Exercise/01. Person/Person.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Person
+{
+    public class Person
+    {
+        public int age;
+        public string name;
+
+        public Person(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public override string ToString()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.Append(String.Format("Name: {0}, Age: {1}",
+                                 this.Name,
+                                 this.Age));
+
+            return stringBuilder.ToString();
+
+        }
+    }
+}
---
+++ C# OOP/01.Inheritance/Inheritance - Exercise/01. Person/StartUp.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Person
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+
+            if (age < 0)
+            {
+                return;
+            }
+
+            Person person;
+
+            if (age <= 15)
+            {
+                person = new Child(name, age);
+            }
+            else
+            {
+                person = new Person(name, age);
+            }
+            Console.WriteLine(person);
+        }
+    }
+}
