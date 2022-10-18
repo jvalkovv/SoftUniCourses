fb70ca20376a5625f494da4b9983feb31d875d35
Yordan Valkov <yordan.valkov@gmail.com>
2022-10-18 13:45:29 +0300

    C# OOP

---
+++ C# OOP/01.Inheritance/Inheritance - Exercise/05. Restaurant/Cake.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Restaurant
+{
+    public class Cake : Dessert
+    {
+        private const decimal CakePrice = 5m;
+        private const double CakeGrams = 250;
+        private const double CakeCalories = 1000;
+        public Cake(string name)
+            : base(name, CakePrice, CakeGrams, CakeCalories)
+        {
+        }
+    }
+}
