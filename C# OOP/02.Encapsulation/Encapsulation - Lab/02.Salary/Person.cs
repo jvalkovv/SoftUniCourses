8c6a151ffcbf2a337cc76ad15d99d4f7a0e6b370
Yordan Valkov <yordan.valkov@gmail.com>
2022-10-18 14:36:23 +0300

    C# OOP

---
+++ C# OOP/02.Encapsulation/Encapsulation - Lab/02.Salary/Person.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonsInfo
+{
+    public class Person
+    {
+        public Person(string firstName, string lastName, int age, decimal salary)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+            this.Salary = salary;
+        }
+        public string FirstName { get; private set; }  
+        public string LastName { get; private set; }
+        public int Age { get; set; }
+        public decimal Salary { get; private set; }
+
+        public void IncreaseSalary(decimal percentage)
+        {
+            if (this.Age < 30)
+            {
+               this.Salary = this.Salary * (percentage/2)/100 + this.Salary;
+            }
+            else
+            {
+                this.Salary = this.Salary * percentage/100 + this.Salary;
+            }
+
+        }
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"{this.FirstName} {this.LastName} receives {this.Salary:f2} leva.");
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
