e77e3bfb63ff66221de380c47d35d1cc18360ac7
Yordan Valkov <yordan.valkov@gmail.com>
2022-10-18 13:28:39 +0300

    C# OOP

---
+++ C# OOP/01.Inheritance/Inheritance - Exercise/06. Animals/Animal.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Animals
+{
+    public class Animal
+    {
+        private string name;
+
+        private int age;
+
+        private string gender;
+
+        public Animal(string name, int age, string gender)
+        {
+            Name = name;
+            Age = age;
+            Gender = gender;
+        }
+
+        public string Name
+        { 
+            get { return this.name; }
+            set
+            {
+                if (char.IsDigit(value[0]) == true)
+                {
+                    throw new ArgumentException("Invalid input!");
+                }
+                this.name = value;
+            }
+        }
+
+        public int Age
+        {
+            get
+            {
+                return this.age;
+            }
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Invalid input!");
+                }
+                this.age = value;
+            }
+        }
+
+        public string Gender
+        {
+            get
+            {
+                return this.gender;
+            }
+            set
+            {
+                if (value == "Male" || value == "Female")
+                {
+                    this.gender = value;
+                }
+                else
+                {
+                    throw new ArgumentException("Invalid input!");
+                }
+            }
+        }
+
+        public  virtual string ProduceSound()
+        {
+            return base.ToString();
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"{GetType().Name}");
+            sb.AppendLine($"{this.Name} {this.Age} {this.Gender}");
+            sb.AppendLine($"{ProduceSound()}");
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
---
+++ C# OOP/01.Inheritance/Inheritance - Exercise/06. Animals/StartUp.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace Animals
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            string cmd = Console.ReadLine();
+            Animal animalType;
+            while (cmd != "Beast!")
+            {
+                try
+                {
+                    if (cmd == "Cat")
+                    {
+                        cmd = Console.ReadLine();
+                        string[] action = cmd.Split();
+
+                        string name = action[0];
+                        int age = int.Parse(action[1]);
+                        string gender = action[2];
+                        if (name == "Tom")
+                        {
+                            animalType = new Tomcat(name, age);
+                            Console.WriteLine(animalType.ToString());
+                        }
+                        else if (name == "Kitten")
+                        {
+                            animalType = new Kitten(name, age);
+                            Console.WriteLine(animalType.ToString());
+                        }
+                        else
+                        {
+                            animalType = new Cat(name, age, gender);
+                        }
+
+                    }
+                    if (cmd == "Dog")
+                    {
+                        cmd = Console.ReadLine();
+                        string[] action = cmd.Split();
+
+                        string name = action[0];
+                        int age = int.Parse(action[1]);
+                        string gender = action[2];
+
+                        animalType = new Dog(name, age, gender);
+                        Console.WriteLine(animalType.ToString());
+                    }
+                    if (cmd == "Frog")
+                    {
+
+                        cmd = Console.ReadLine();
+                        string[] action = cmd.Split();
+
+                        string name = action[0];
+                        int age = int.Parse(action[1]);
+                        string gender = action[2];
+
+                        animalType = new Frog(name, age, gender);
+                        Console.WriteLine(animalType.ToString());
+                    }
+
+                    cmd = Console.ReadLine();
+
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+        }
+    }
+}
