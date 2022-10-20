63545a4e25cff9b47f815bf47cb12e824b6e06bf
Yordan Valkov <yordan.valkov@gmail.com>
2022-10-20 21:44:16 +0300

    C# OOP

---
+++ C# OOP/02.Encapsulation/Encapsulation - Exercise/04.PizzaCalories/Program.cs
@@ -0,0 +1,79 @@
+﻿using PizzaCalories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PizzaCalories
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pizza pizza;
+            Dough dough;
+
+            try
+            {
+                string[] pizzaInfo = Console.ReadLine().Split();
+                string pizzaName = pizzaInfo[1];
+                pizza = new Pizza(pizzaName);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return;
+            }
+
+            try
+            {
+                string[] doughInfo = Console.ReadLine().Split();
+                string flour = doughInfo[1];
+                string bakingTech = doughInfo[2];
+                double weight = double.Parse(doughInfo[3]);
+                dough = new Dough(flour.ToLower(), bakingTech.ToLower(), weight);
+
+                pizza.PizzaDough = dough;
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return;
+            }
+
+            string toppingInfo;
+
+            while ((toppingInfo = Console.ReadLine()) != "END")
+            {
+                try
+                {
+                    string[] toppingParams = toppingInfo.Split();
+
+                    string toppingName = toppingParams[1].ToLower();
+                    double toppingWeight = double.Parse(toppingParams[2]);
+                    Topping topping = new Topping(toppingName, toppingWeight);
+
+                    pizza.Add(topping);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                    return;
+                }
+            }
+
+            try
+            {
+                Console.WriteLine($"{pizza.Name} - {pizza.PizzaCalories():f2} Calories.");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return;
+            }
+        }
+    }
+}
+   
