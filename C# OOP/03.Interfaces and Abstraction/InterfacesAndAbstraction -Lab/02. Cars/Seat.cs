993794e2a7b49613fd195c5b7d209291c2eeba35
Yordan Valkov <yordan.valkov@gmail.com>
2022-10-22 09:13:11 +0300

    C# OOP

---
+++ C# OOP/03.Interfaces and Abstraction/InterfacesAndAbstraction -Lab/02. Cars/Seat.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cars
+{
+    public class Seat : ICar
+    {
+        public string Model { get; set; }
+        public string Color { get; set; }
+
+        public Seat(string model, string color)
+        {
+            this.Model = model;
+            this.Color = color;
+        }
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"{this.Color} {this.GetType().Name} {this.Model}");
+            sb.AppendLine(Start());
+            sb.AppendLine(Stop());
+            return sb.ToString().TrimEnd();
+        }
+
+        public string Start()
+        {
+            return "Engine start";
+        }
+
+        public string Stop()
+        {
+            return "Breaaak!";
+        }
+    }
+}
---
+++ C# OOP/03.Interfaces and Abstraction/InterfacesAndAbstraction -Lab/02. Cars/Tesla.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cars
+{
+    public class Tesla : ICar, IElectricCar
+    {
+        public string Model { get ; set; }
+        public string Color { get; set ; }
+        public int Battery { get; set; }
+
+        public Tesla(string model, string color, int battery)
+        {
+            Model = model;
+            Color = color;
+            Battery = battery;
+        }
+ 
+       
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"{this.Color} {this.GetType().Name} {this.Model} with {this.Battery} Batteries");
+            sb.AppendLine(Start());
+            sb.AppendLine(Stop());
+            return sb.ToString().TrimEnd();
+        }
+
+        public string Start()
+        {
+            return "Engine start";
+        }
+
+        public string Stop()
+        {
+            return "Breaaak!";
+        }
+    }
+}
