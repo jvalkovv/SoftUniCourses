e7acfa86fd7c73f187bd4d436ddafcfc84a5d7b1
Yordan Valkov <yordan.valkov@gmail.com>
2022-10-23 19:37:00 +0300

    C# OOP

---
+++ C# OOP/03.Interfaces and Abstraction/InterfacesAndAbstraction -Exercise/01.DefineAnInterfaceIPerson/Citizen.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonInfo
+{
+    public class Citizen : IPerson
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public Citizen(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+    }
+}
