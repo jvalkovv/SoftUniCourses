dc36e212350a9763dcb26fee9784c3b347e33622
Yordan Valkov <yordan.valkov@gmail.com>
2022-11-03 11:45:51 +0200

    C# OOP

---
+++ C# OOP/05.Exceptions and Error Handling/02.EnterNumbers/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace EnterNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int start = 1;
+            int end = 100;
+            int counter = 0;
+            int num = 0;
+            int[] numbers = new int[10];
+
+            while (counter < 10)
+            {
+                try
+                {
+                    num = ReadNumber(start, end);
+                    start = num;
+                    numbers[counter] = num;
+                    counter++;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Invalid Number!");
+                }
+                catch (ArgumentException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+
+            Console.WriteLine(String.Join(", ", numbers));
+        }
+        public static int ReadNumber(int start, int end)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            if (num <= start || num >= end)
+            {
+                throw new ArgumentException($"Your number is not in range {start} - 100!");
+            }
+
+            return num;
+        }
+    }
+}
---
+++ C# OOP/05.Exceptions and Error Handling/03.Cards/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Cards
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> card = new Dictionary<string, string>();
+            string[] validFacesCard = { "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A" };
+            string[] validSuitCard = { "S", "H", "D", "C" };
+            string[] input = Console.ReadLine()
+                .Split(", ").ToArray();
+
+            string result;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                try
+                {
+                    string[] cardInfo = input[i].Split();
+                    string faceCard = cardInfo[0];
+                    string suitCard = cardInfo[1];
+
+                    if (!validFacesCard.Contains(faceCard) || !validSuitCard.Contains(suitCard))
+                    {
+                        throw new ArgumentException("Invalid card!");
+                    }
+                    else
+                    {
+                        if (suitCard == "S")
+                        {
+                            result = "\u2660";
+                            card.Add(faceCard, result);
+                        }
+                        else if (suitCard == "H")
+                        {
+                            result = "\u2665";
+                            card.Add(faceCard, result);
+                        }
+                        else if (suitCard == "D")
+                        {
+                            result = "\u2666";
+                            card.Add(faceCard, result);
+                        }
+                        else if (suitCard == "C")
+                        {
+                            result = "\u2663";
+                            card.Add(faceCard, result);
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+
+            foreach (var item in card)
+            {
+                Console.Write($"[{item.Key}{item.Value}] ");
+            }
+
+        }
+    }
+}
