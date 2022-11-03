1e4bdb7d89f499b84db91615d091167b5ea7e8c3
Yordan Valkov <yordan.valkov@gmail.com>
2022-11-03 11:28:01 +0200

    C# OOP

---
+++ C# OOP/04.Polymorphism/Polymorphism - Lab/02.Animals/Animal.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Animals
+{
+    public class Animal
+    {
+        private string name;
+
+        private string favouriteFood;
+
+        public Animal(string name, string food)
+        {
+            this.name = name;
+            favouriteFood = food;
+        }
+
+        public virtual string ExplainSelf()
+        {
+            return $"I am {this.name} and my fovourite food is {this.favouriteFood}";
+        }
+
+    }
+}
---
+++ C# OOP/04.Polymorphism/Polymorphism - Lab/02.Animals/Cat.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Animals
+{
+    public class Cat : Animal
+    {
+        public Cat(string name, string food) : base(name, food)
+        {
+        }
+
+        public override string ExplainSelf()
+        {
+            return base.ExplainSelf() + Environment.NewLine + "MEEOW";
+        }
+
+    }
+}
