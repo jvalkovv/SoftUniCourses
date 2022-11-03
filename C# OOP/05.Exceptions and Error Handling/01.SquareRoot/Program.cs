e6a823498a0eebea425061bde22bf2208ee032ca
Yordan Valkov <yordan.valkov@gmail.com>
2022-11-03 11:33:58 +0200

    C# OOP

---
+++ C# OOP/05.Exceptions and Error Handling/01.SquareRoot/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace SquareRoot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double number = double.Parse(Console.ReadLine());
+            try
+            {
+                ValidSqr(number);
+                Console.WriteLine(ValidSqr(number));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Goodbye.");
+            }
+
+
+        }
+
+        public static double ValidSqr(double num)
+        {
+            if (num < 0)
+            {
+                throw new ArgumentException("Invalid number.");
+            }
+
+            return Math.Sqrt(num);
+        }
+    }
+}
