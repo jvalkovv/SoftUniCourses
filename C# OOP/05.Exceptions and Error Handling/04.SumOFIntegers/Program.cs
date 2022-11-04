028a5878820ba536b99d4bdd35646b3c3d697d0e
Yordan Valkov <yordan.valkov@gmail.com>
2022-11-04 22:17:49 +0200

    C# OOP

---
+++ C# OOP/05.Exceptions and Error Handling/04.SumOFIntegers/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace SumOfIntegers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split();
+            int sum = 0;
+            foreach (var item in input)
+            {
+                try
+                {
+                    int number = Int32.Parse(item);
+                    sum += Int32.Parse(item);
+                    Console.WriteLine
+                        ($"Element '{item}' processed - current sum: {sum}");
+
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine($"The element '{item}' is in wrong format!");
+                    Console.WriteLine($"Element '{item}' processed - current sum: {sum}");
+                }
+                catch (OverflowException)
+                {
+                    Console.WriteLine($"The element '{item}' is out of range!");
+                    Console.WriteLine($"Element '{item}' processed - current sum: {sum}");
+                }
+            }
+            Console.WriteLine($"The total sum of all integers is: {sum}");
+        }
+    }
+}
