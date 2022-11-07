0e8a3ee44842d52780ff00a28c173381b236373a
Yordan Valkov <yordan.valkov@gmail.com>
2022-11-07 23:21:59 +0200

    C# OOP

---
+++ C# OOP/07.Unit Testing/Unit Testing - Lab/02.Dummy Tests/DummyTests.cs
@@ -0,0 +1,52 @@
+﻿using NUnit.Framework;
+using System;
+
+[TestFixture]
+public class DummyTests
+{
+    [Test]
+    public void DummyLosesHealthIfAttacked()
+    {
+        Axe axe = new Axe(10, 10);
+        Dummy dummy = new Dummy(10, 10);
+
+        dummy.TakeAttack(axe.AttackPoints);
+
+        Assert.That(dummy.Health, Is.EqualTo(0), "Axe Durability doesn't change after attach.");
+    }
+
+    [Test]
+    public void TestAttackWithZeroHealth()
+    {
+        Axe axe = new Axe(10, 10);
+        Dummy dummy = new Dummy(10, 10);
+
+        dummy.TakeAttack(axe.AttackPoints);
+
+        Assert.Throws<InvalidOperationException>(() => dummy.TakeAttack(axe.AttackPoints));
+    }
+
+    [Test]
+    public void AliveDummyCantGiveExperience()
+    {
+        Axe axe = new Axe(10, 10);
+        Dummy dummy = new Dummy(12, 10);
+
+        dummy.TakeAttack(axe.AttackPoints);
+
+        Assert.Throws<InvalidOperationException>(() => dummy.GiveExperience());
+    }
+
+    [Test]
+    public void DeadDummyCanGiveXP()
+    {
+        Axe axe = new Axe(10, 10);
+        Dummy dummy = new Dummy(10, 10);
+
+        dummy.TakeAttack(axe.AttackPoints);
+
+        Assert.That(() => dummy.GiveExperience(), Is.EqualTo(10));
+    }
+
+
+}
