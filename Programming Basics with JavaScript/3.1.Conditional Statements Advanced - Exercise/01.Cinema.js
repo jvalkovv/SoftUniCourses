039c7501bff2bb0b363aee9d60a64eccdecee8d8
Yordan Valkov <yordan.valkov@gmail.com>
2023-05-09 17:39:53 +0300

    Programming Basics with JavaScript

---
+++ Programming Basics with JavaScript/3.1.Conditional Statements Advanced - Exercise/01.Cinema.js
@@ -0,0 +1,21 @@
+function tickets(params) {
+  let typeOfProjection = String(params[0]);
+  let numberOfRows = Number(params[1]);
+  let numberOfCol = Number(params[2]);
+
+  let price = 0;
+
+  if (typeOfProjection === "Premiere") {
+    price = 12.0;
+    price *= numberOfRows * numberOfCol;
+  } else if (typeOfProjection === "Normal") {
+    price = 7.5;
+    price *= numberOfRows * numberOfCol;
+  } else if (typeOfProjection === "Discount") {
+    price = 5;
+    price *= numberOfCol * numberOfRows;
+  }
+
+  console.log(`${price.toFixed(2)} leva`);
+}
+tickets(["Premiere", "10", "12"]);
