a3bd5a683546fc54132b1843532c5224f323c24f
Yordan Valkov <yordan.valkov@gmail.com>
2023-05-14 17:11:03 +0300

    Programming Basics with JavaScript

---
+++ Programming Basics with JavaScript/4. For Loop - Lab/05.CharacterSequence.js
@@ -0,0 +1,10 @@
+function sequence(params) {
+    
+    let character = String(params[0]);
+
+    for (let index = 0; index < character.length; index++) {
+      console.log(character[index]);
+        
+    }
+}
+sequence(["softuni"]);
