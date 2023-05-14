0a21eef233761bd891db233c291c88d5eb1738e8
Yordan Valkov <yordan.valkov@gmail.com>
2023-05-14 15:18:54 +0300

    Programming Basics with JavaScript

---
+++ Programming Basics with JavaScript/4. For Loop - Lab/02.Numbers N...1.js
@@ -0,0 +1,9 @@
+function reverseNumbers(input) {
+    
+    let num = Number(input[0]);
+
+    for (let index = num; index > 0; index--) {
+       console.log(index);       
+    }
+}
+reverseNumbers('5');
