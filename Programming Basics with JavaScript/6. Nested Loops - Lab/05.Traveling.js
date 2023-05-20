657a8de3a78ffb0b0d288fa27f4212e47aea434a
Yordan Valkov <yordan.valkov@gmail.com>
2023-05-20 15:13:42 +0300

    Programming Basics with JavaScript

---
+++ Programming Basics with JavaScript/6. Nested Loops - Lab/05.Traveling.js
@@ -0,0 +1,32 @@
+function traveling(params) {
+    let destination = params.shift();
+    let neededMoney = params.shift();
+    while (destination !== "End") {
+      let currentSavedMoney = 0;
+      while (currentSavedMoney < neededMoney) {
+        let savedMoney = Number(params.shift());
+        currentSavedMoney += savedMoney;
+      }
+      console.log(`Going to ${destination}!`);
+      destination = params.shift();
+      neededMoney = params.shift();
+    }
+  }
+  traveling([
+    "Greece",
+    "1000",
+    "200",
+    "200",
+    "300",
+    "100",
+    "150",
+    "240",
+    "Spain",
+    "1200",
+    "300",
+    "500",
+    "193",
+    "423",
+    "End",
+  ]);
+  
