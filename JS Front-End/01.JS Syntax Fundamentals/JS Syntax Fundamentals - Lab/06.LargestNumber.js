00f7ede7d5feb105a4b9ab4eff2ae3ae4fbf588a
Yordan Valkov <yordan.valkov@gmail.com>
2023-06-19 13:13:48 +0300

    JS Syntax Fundamentals

---
+++ JS Front-End/01.JS Syntax Fundamentals/JS Syntax Fundamentals - Lab/06.LargestNumber.js
@@ -0,0 +1,12 @@
+function solve(n1, n2, n3) {
+  let result;
+  if (n1 > n2 && n1 > n3) {
+    result = n1;
+  } else if (n2 > n1 && n2 > n3) {
+    result = n2;
+  } else if (n3 > n1 && n3 > n2) {
+    result = n3;
+  }
+  console.log(`The largest number is ${result}.`);
+}
+solve(5, -3, 16);
