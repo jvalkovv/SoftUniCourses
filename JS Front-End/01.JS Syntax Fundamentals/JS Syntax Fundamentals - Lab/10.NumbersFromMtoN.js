698d90e51593541bd1c8bb8d4a4d6f4db981ce61
Yordan Valkov <yordan.valkov@gmail.com>
2023-06-20 13:57:12 +0300

    JS Syntax Fundamentals

---
+++ JS Front-End/01.JS Syntax Fundamentals/JS Syntax Fundamentals - Lab/10.NumbersFromMtoN.js
@@ -0,0 +1,6 @@
+function solve(number1, number2) {
+  for (let index = number1; index >= number2; index--) {
+    console.log(index);
+  }
+}
+solve(6, 2);
---
+++ JS Front-End/01.JS Syntax Fundamentals/JS Syntax Fundamentals - Lab/11.SumFirstAndLastArrayElements.js
@@ -0,0 +1,7 @@
+function solve(params) {
+  let first = params[0];
+  let last = params[params.length - 1];
+
+  console.log(first + last);
+}
+solve([20, 30, 40]);
