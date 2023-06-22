72d5faceaf7bde1bcc76cf24c4afb166bc891924
Yordan Valkov <yordan.valkov@gmail.com>
2023-06-22 19:00:15 +0300

    Functions and Statements

---
+++ JS Front-End/02.Functions and Statements/Functions and Statements - Exercises/06.PasswordValidator.js
@@ -0,0 +1,35 @@
+function solve(params) {
+  let digitCount = 0;
+  let isValid = true;
+  for (let char of params) {
+    char = Number(char);
+    if (isDigit(char)) {
+      digitCount++;
+    }
+  }
+  for (let char of params) {
+    if (!isLetter(char) && !isDigit(char)) {
+      console.log(`Password must consist only of letters and digits`);
+      isValid = false;
+      break;
+    }
+  }
+  if (params.length < 6 || params.length > 10) {
+    console.log(`Password must be between 6 and 10 characters`);
+    isValid = false;
+  }
+  if (digitCount < 2) {
+    console.log(`Password must have at least 2 digits`);
+    isValid = false;
+  }
+  if (isValid) {
+    console.log(`Password is valid`);
+  }
+  function isDigit(char) {
+    return char >= 0 && char <= 9;
+  }
+  function isLetter(char) {
+    return (char >= "a" && char <= "z") || (char >= "A" && char <= "Z");
+  }
+}
+solve("Pass$word");
