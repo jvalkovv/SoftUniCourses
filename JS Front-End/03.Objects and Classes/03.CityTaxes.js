b35f77be7261310d342503133b7b0347bfc0c9db
Yordan Valkov <yordan.valkov@gmail.com>
2023-07-02 11:43:58 +0300

    Objects and Classes

--- JS Front-End/03.Objects and Classes/03.CityTaxes.js
+++ JS Front-End/03.Objects and Classes/03.CityTaxes.js
@@ -5,13 +5,13 @@ function cityTaxes(name, population, treasury) {
     treasury,
     taxRate: 10,
     collectTaxes() {
-      this.treasury += Math.ceil(this.population * this.taxRate);
+      this.treasury += Math.trunc(this.population * this.taxRate);
     },
     applyGrowth(percent) {
-      this.population += Math.ceil((this.population * percent) / 100);
+      this.population += Math.trunc((this.population * percent) / 100);
     },
     applyRecession(percent) {
-      this.treasury -= Math.ceil((this.treasury * percent) / 100);
+      this.treasury -= Math.trunc((this.treasury * percent) / 100);
     },
   };
   return city;
